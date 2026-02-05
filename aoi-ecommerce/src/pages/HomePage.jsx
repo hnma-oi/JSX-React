2eee0c9fb1796025ebec1d55ed7cba79e428c17d
日南葵 <johndoe@email.com>
2026-02-05 20:48:32 +0700

    Moved header to a shared components.

--- aoi-ecommerce/src/pages/HomePage.jsx
+++ aoi-ecommerce/src/pages/HomePage.jsx
@@ -1,42 +1,12 @@
-import './header.css';
+import { Header } from "../components/Header";
 import './HomePage.css';
 
 
 export function HomePage() {
     return (
         <>
-            <title>Homepage</title>
-            <div className="header">
-                <div className="left-section">
-                    <a href="/" className="header-link">
-                    <img className="logo"
-                        src="images/logo-white.png" />
-                    <img class="mobile-logo"
-                        src="images/mobile-logo-white.png" />
-                    </a>
-                </div>
+            <Header />
 
-                <div class="middle-section">
-                    <input class="search-bar" type="text" placeholder="Search" />
-
-                    <button class="search-button">
-                    <img class="search-icon" src="images/icons/search-icon.png" />
-                    </button>
-                </div>
-
-                <div class="right-section">
-                    <a class="orders-link header-link" href="/orders">
-
-                    <span class="orders-text">Orders</span>
-                    </a>
-
-                    <a class="cart-link header-link" href="/checkout">
-                    <img class="cart-icon" src="images/icons/cart-icon.png" />
-                    <div class="cart-quantity">3</div>
-                    <div class="cart-text">Cart</div>
-                    </a>
-                </div>
-            </div>
             <div className="home-page">
                 <div className="products-grid">
                     <div className="product-container">
