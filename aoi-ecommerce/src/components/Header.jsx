3552b9a2a88a39c51360666bbb846e1b6faeee8b
日南葵 <johndoe@email.com>
2026-02-07 04:12:08 +0700

    added cart quantity from backend

--- aoi-ecommerce/src/components/Header.jsx
+++ aoi-ecommerce/src/components/Header.jsx
@@ -5,7 +5,12 @@ import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import './header.css';
 
-export function Header() {
+export function Header({ cart }) {
+    let TotalQuantity = 0;
+    cart.forEach((cartItem) => {
+        TotalQuantity += cartItem.quantity;
+    })
+
     return (
         <div className="header">
             <div className="left-section">
@@ -33,7 +38,7 @@ export function Header() {
 
                 <NavLink className="cart-link header-link" to="/checkout">
                     <img className="cart-icon" src={CartIcon} />
-                    <div className="cart-quantity">3</div>
+                    <div className="cart-quantity">{TotalQuantity}</div>
                     <div className="cart-text">Cart</div>
                 </NavLink>
             </div>
--- aoi-ecommerce/src/pages/HomePage.jsx
+++ aoi-ecommerce/src/pages/HomePage.jsx
@@ -6,13 +6,20 @@ import CheckmarkIcon from '../assets/images/icons/checkmark.png';
 import './HomePage.css';
 
 export function HomePage() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     axios.get('http://localhost:3000/api/products')
       .then((response) => {
     setProducts(response.data);
-  });
-  }, []);
+    });
+
+    axios.get('http://localhost:3000/api/cart-items')
+      .then((response) => {
+    setCart(response.data)
+    })
+}, []);
   
 
   return (
@@ -20,7 +27,7 @@ export function HomePage() {
       <title>Ecommerce Project</title>
       <link rel="icon" type="image/svg+xml" href="anna.webp" />
 
-      <Header />
+      <Header cart={cart} />
 
       <div className="home-page">
         <div className="products-grid">
